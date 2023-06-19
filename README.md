486d9b09407480ac5993241a2585b7bd17449251
anaximeno <anaximenobrito@gmail.com>
2023-06-19 15:20:04 -0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OffRoad - Computing Offloader
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    printf("OffRoad - Computing Offloader\n");
+    return 0;
+}
